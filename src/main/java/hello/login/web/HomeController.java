789a1979f5b7cc208edc2b607676c4c32d14fac4
정현준 <jeongcode@naver.com>
2022-07-09 19:09:16 +0900

    @SessionAttribute 적용

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -51,7 +52,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
         HttpSession session = request.getSession(false);
         if(session == null){
@@ -60,6 +61,16 @@ public class HomeController {
 
         // 세션이 유지되면 로그인으로 이동
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        model.addAttribute(loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER , required = false) Member loginMember , Model model){
+        if(loginMember == null){
+            return "home";
+        }
+
         model.addAttribute(loginMember);
         return "loginHome";
     }
